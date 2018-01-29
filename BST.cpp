c908ddf22afbea8f475b528fb8e29cb0fd55209a
anubhav253 <anubhav253kumar@gmail.com>
2018-01-29 14:46:19 +0530

    Height of tree and size of tree function working properly

--- BST.cpp
+++ BST.cpp
@@ -118,62 +118,6 @@ class Tree{
  			return 1 + sizeTree(curr->getLeft()) + sizeTree(curr->getRight());
  		}
  		
- 		int leafNode(TreeNode* curr){
- 			if(curr == NULL) return 0;
- 			if(curr->getLeft() == NULL && curr->getRight() == NULL) return 1;
- 			return leafNode(curr->getLeft()) + leafNode(curr->getRight());
- 		}
- 		
- 		int TwoChild(TreeNode* curr){
- 			if(curr == NULL) return 0;
-			return (curr->getLeft() != NULL && curr->getRight() != NULL) + TwoChild(curr->getLeft()) + TwoChild(curr->getRight());
- 		}
- 		
- 		int OneChild(TreeNode* curr){
- 			if(curr == NULL) return 0;
- 			return (curr->getLeft() != NULL ^ curr->getRight() != NULL) + OneChild(curr->getLeft()) + OneChild(curr->getRight());
- 		}
- 		
- 		int SumTree(TreeNode* curr){
- 			if(curr == NULL) return 0;
- 			return curr->getVal() + SumTree(curr->getLeft()) + SumTree(curr->getRight());
-  		}
- 		
- 		bool searchTree(TreeNode* curr, int val){
- 			if(curr ==NULL) return 0;
-			if(val == curr->getVal()) return 1; 			
-			if(val > curr->getVal()) return searchTree(curr->getRight(),val);
-			return searchTree(curr->getLeft(),val);
- 		}
- 		
- 		void invertTree(TreeNode* curr){
- 			TreeNode* temp;
- 			if(curr==NULL)return;
- 			invertTree(curr->getLeft());
- 			invertTree(curr->getRight());
- 			temp = curr->getLeft();
- 			curr->setLeft(curr->getRight());
- 			curr->setRight(temp);
- 		}
-
- 		
- 		void minNode(TreeNode* curr){
- 			while(curr->getLeft() != NULL){
- 				curr = curr->getLeft();
- 			}
- 		}
- 		
- 		void deleteNode(TreeNode* curr, int val){
- 			if(curr==NULL) return;
- 			if(val == curr->getVal()){
- 				
- 			}
- 			else if(val < curr->getVal()){
- 				deleteNode(curr->getLeft(),val);
- 			} else {
- 				deleteNode(curr->getRight(),val);
- 			}
- 		}
 };
 
 
@@ -182,7 +126,7 @@ int main(){
 	TreeNode* tempRoot;
 	int choice,value;
 	while(1){
-		cout<<"\n1.Insert element to the tree\n2.Traverse the tree\n3.Delete\n6.Exit\nEnter your choice:";
+		cout<<"\n1.Insert element to the tree\n2.Traverse the tree\n3.Delete\n4.Height of tree\n5.Size of tree\n6.Exit\nEnter your choice:";
 		cin>>choice;
 		switch(choice){
 			case 1:
@@ -197,6 +141,14 @@ int main(){
 				t->printPreOrder(t->getRoot());
 				cout<<"\nPostorder: "<<endl;
 				t->printPostOrder(t->getRoot());
+				break;
+			case 4:
+				cout<<"\nHeight of tree is:"<<endl;
+				cout<<t->heightTree(t->getRoot())<<endl;
+				break;
+			case 5:
+				cout<<"\nSize of tree is:"<<endl;
+				cout<<t->sizeTree(t->getRoot())<<endl;
 				break;
 			case 6:
 				exit(0);
